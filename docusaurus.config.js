bdcce5e7628c5c2614a26486366e1d34b91d4f38
Nitin Kumar <snitin315@gmail.com>
2022-11-26 16:49:55 +0530

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'snitin315', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
